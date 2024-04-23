77f909b4de9f8224cfd65aaf1c261962e5f6ad94
Leon Dick <ldick@2-B-1.42heilbronn.de>
2024-04-23 19:21:04 +0200

    mlkm

--- test.c
+++ test.c
@@ -6,39 +6,30 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 20:09:59 by ldick             #+#    #+#             */
-/*   Updated: 2024/04/22 20:41:26 by ldick            ###   ########.fr       */
+/*   Updated: 2024/04/23 19:20:28 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "game.h"
 
-char *map(t_vars *vars, char *map)
+int make_matric(char *map)
 {
-	char	*line;
-	int		fd;
-	char *milk;
-	int x;
-	int y;
-	char map[x][y];
-	int i;
-	
-	i = 0;
-	fd = open(map, O_RDONLY);
-	line = get_next_line(fd);
-	milk = ft_strdup(line);
-	while(milk != '\0')
-	{
-		map[x][y] == milk[i]
-	}
-	// while (line != NULL)
-	// {
-		
-	// 	free(line);
-	// 	line = get_next_line(fd);
-	// }
-	printf("%s", line); 
-	close(fd);
-	return (0);
+	char **mapp;
+
+	mapp = ft_split(map, '\n');
+	int x = 0;
+	int y = 0;
+while (mapp[y] != NULL) { // Check if the pointer mapp[y] is not NULL
+        x = 0; // Reset x for each new line
+        while (mapp[y][x] != '\0') { // Loop through each character until end of string
+            printf("%c", mapp[y][x]);
+            x++;
+        }
+        printf("\n"); // Print a newline after each line
+        y++;
+    }
+printf("%d--%d", x, y);
+	return (1);
 }
 
 int main(int argc, char *argv[])
@@ -46,6 +37,7 @@ int main(int argc, char *argv[])
 	t_vars *vars;
 
 	vars = malloc(sizeof(t_vars));
-	map(vars, argv[1]);
+	read_map_file(vars, argv[1]);
+	make_matric(vars->buffer);
 	return (0);
 }
